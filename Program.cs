edc1fe3ac4c82bfbafd9b7cdcaa3cd1a3c3c907b
RChant17 <st10481221@rcconnect.edu.za>
2026-04-13 19:12:51 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+
+    {
+
+
+        Console.Title = "Chant-Bot - Cybersecurity Awareness";
+
+        ConsoleUI.DisplayLogo();
+
+        UserProfile user = new UserProfile();
+        user.GetUserName();
+
+        ConsoleUI.BotSay(
+            $"Hello {user.Name}! Welcome to the Cybersecurity Awareness Bot\n" +
+            "I am Chant-Bot, here to help you stay safe online.\n" +
+            "Type 'exit' or 'bye' to quit."
+        );
+
+        AudioPlayer.Speak("Welcome to the Cybersecurity Awareness Bot");
+
+        ResponsesService bot = new ResponsesService();
+        bot.RunChat(user.Name);
+    }
+}
